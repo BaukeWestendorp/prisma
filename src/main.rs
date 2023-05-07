099dd5fa847e286c5ac7181151cbe27e257dd863
BaukeWestendorp <baukewestendorp@gmail.com>
2023-05-07 20:06:14 +0200

    Update sample project

--- src/main.rs
+++ src/main.rs
@@ -14,28 +14,32 @@ mod runner;
 fn main() {
     let project: Project = serde_json::from_str(include_str!("project.json")).unwrap();
 
-    let runner = Arc::new(Mutex::new(Runner::new(project, "localhost:7200")));
+    let runner = Arc::new(Mutex::new(Runner::new(
+        project,
+        // "localhost:7200",
+        "kilowatt-cruiser.local:80",
+    )));
 
-    std::thread::spawn({
-        let runner = runner.clone();
-        move || {
-            let new_project = Project {
-                framerate: 60,
-                global_bpm: 60.0,
-                effects: vec![EffectWrapper {
-                    bpm_factor: 1.0,
-                    range: LedRange { min: 0, max: 60 },
-                    effect: Effect::StaticColor {
-                        color: Color::white(),
-                    },
-                }],
-            };
+    // std::thread::spawn({
+    //     let runner = runner.clone();
+    //     move || {
+    //         let new_project = Project {
+    //             framerate: 60,
+    //             global_bpm: 60.0,
+    //             effects: vec![EffectWrapper {
+    //                 bpm_factor: 1.0,
+    //                 range: LedRange { min: 0, max: 60 },
+    //                 effect: Effect::StaticColor {
+    //                     color: Color::white(),
+    //                 },
+    //             }],
+    //         };
 
-            std::thread::sleep(Duration::from_millis(1000));
+    //         std::thread::sleep(Duration::from_millis(1000));
 
-            runner.lock().unwrap().update_project(new_project);
-        }
-    });
+    //         runner.lock().unwrap().update_project(new_project);
+    //     }
+    // });
 
     loop {
         runner.lock().unwrap().proceed();
